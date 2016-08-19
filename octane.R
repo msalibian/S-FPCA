4d46c5bcc353851034342cb42a1e7f8db0c7cf24
Matias Salibian Barrera <matias@stat.ubc.ca>
2016-08-19 10:11:17 -0700

    updated analysis

--- octane.R
+++ octane.R
@@ -6,6 +6,8 @@
 ## wavelengths
 ## samples 25, 26, 36:39 contain added alcohol
 
+# Combinations that work
+# octane-q2.R
 
 source('S-FPCA-functions.R')
 library(mvoutlier)
@@ -24,6 +26,7 @@ q <- 6
 Ncand <- 1000
 x.s <- sfpca.par(x=x, mu=mui, q=q, Ncand=Ncand, seed=123, init.it=50, max.it=500, 
                  tol=1e-6, trace=FALSE, tuning.rho=3, bb = 0.2426) 
+# cc=1.54764, b=.5
 mu.hat <- x.s$mu
 xc <- scale(x, center=x.s$mu, scale=FALSE)
 bon <- qr.Q(qr(x.s$b))
@@ -34,7 +37,7 @@ re.s <- rowMeans((x-x.hat.s)^2)
 outs.true <- c(25, 26, 36:39)
 
 names(re.s) <- 1:nrow(x)
-ouS <- as.numeric(names(adjbox(re.s)$out))
+ouS <- as.numeric(names(adjbox(re.s, plot=FALSE)$out))
 inliers <- re.s[ouS] < median(re.s)
 (outs.s <- ouS[!inliers])
 
